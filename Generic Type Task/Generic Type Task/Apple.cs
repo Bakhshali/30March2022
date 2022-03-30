5b3a9f2f6cdd09a4b126cf1f682df63e0f2f9ceb
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-30 13:50:41 +0400

    Task 1 added

---
+++ Generic Type Task/Generic Type Task/Apple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Type_Task
+{
+    internal class Apple
+    {
+
+        public string Vitamin;
+        public Apple(string vitamin)
+        {
+            Vitamin = vitamin;
+        }
+        public override string ToString()
+        {
+            return $"Vitamin:{Vitamin}";
+        }
+    }
+}
---
+++ Generic Type Task/Generic Type Task/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Generic_Type_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lemon lemon = new Lemon("Vitamin-C");
+            Pineapple lemonApple = new Pineapple("Vitamin-B");
+            Apple apple = new Apple("Vitamin-A");
+
+            Console.WriteLine("LemonList:");
+            BasketList<Lemon> basketList = new BasketList<Lemon>();
+            basketList.AddItem(lemon);
+            foreach (var item in basketList.GetArray())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("PineappleList:");
+
+            BasketList<Pineapple> basketList2 = new BasketList<Pineapple>();
+            basketList2.AddItem(lemonApple);
+            foreach (var item in basketList2.GetArray())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("AppleList:");
+            BasketList<Apple> basketList3 = new BasketList<Apple>();
+            basketList3.AddItem(apple);
+            foreach (var item in basketList3.GetArray())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
