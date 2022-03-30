3dd21e7eb1418d6ff426f204adf4762959062571
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-30 13:51:04 +0400

    Task 2 added

---
+++ Collection Task/Collection Task/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Collection_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList sortedList = new SortedList();
+
+            sortedList.Add(1980,"C++");
+            sortedList.Add(1970, "Pacsal");
+            sortedList.Add(1995, "PHP");
+            sortedList.Add(1966, "JOSS");
+            sortedList.Add(1978, "SQL");
+
+            foreach (DictionaryEntry item in sortedList)
+            {
+
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+        }
+    }
+}
